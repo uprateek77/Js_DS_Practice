f8e41890f98b27958c4bf26a28eae174168db9bf
Prateek_Upadhyay <prateek.upadhyay@mail.vinove.com>
2023-03-29 15:49:39 +0530

    array reverse completed

---
+++ js/arr_rev/arr_rev.js
@@ -0,0 +1,32 @@
+var arr=[];
+var reversedArray=[];
+const handleSubmit=(e)=>{
+    e.preventDefault()
+    val=document.getElementById('val').value;
+    arr.push(Number(val));
+    console.log("Button Clicked")
+    document.getElementById("myText").innerHTML = arr;
+    document.getElementById("myForm").reset();
+}
+
+function reverseArray(){
+    var start=0
+    var end=arr.length-1
+    console.log(start,end)
+    // while (start < end)
+    // {  
+    //     var temp = arr[start];
+    //     arr[start] = arr[end];
+    //     arr[end] = temp;
+    //     start++;
+    //     end--;
+    // }
+    for(let i=0;i<=end;i++){
+        let temp= arr[i]
+        arr[i]=arr[end]
+        arr[end]=temp
+        end=end-1;
+        reversedArray.push(temp)
+    }
+    console.log(arr)
+ }
